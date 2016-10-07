6f5dda247d817b255690a6ebb68262864c823b35
rinalee <mayakorinalee@gmail.com>
2016-10-07 12:07:03 +0200

    Add files via upload

---
+++ CharacterGun/Bullets.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullets : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody r = GetComponent<Rigidbody> ();	//Get Bullet RigidBody
+		r.AddRelativeForce(Vector3.right * 1000f);	//Add force to Bullet
+		StartCoroutine(DestroyBuullet());
+	}
+	IEnumerator DestroyBuullet(){
+		yield return new WaitForSeconds (3f);
+		Destroy (this.gameObject);
+	}
+
+}
---
+++ CharacterGun/Gun.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+	
+	public GameObject Bullet;
+	private Coroutine c;
+
+	void Update() {
+		RotateGun ();
+		if (Input.GetMouseButtonDown (0)) {
+			c = StartCoroutine (SpawnBullet ());
+		}
+		if (Input.GetMouseButtonUp (0)) {
+			StopCoroutine(c);
+		}
+	}
+
+		void RotateGun (){
+		float f = Input.GetAxis ("Vertical");
+		GetComponent<Transform> ().Rotate (Vector3.forward * f);
+	}
+
+		IEnumerator SpawnBullet(){
+			while (true){
+				Instantiate (Bullet,GetComponent<Transform>().position,GetComponent<Transform>().rotation);
+			yield return new WaitForSeconds (0.1f);
+		}
+	}
+}
