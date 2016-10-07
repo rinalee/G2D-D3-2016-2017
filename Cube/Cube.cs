956440692f47cd5af36eab110fcdbf97341df3ed
rinalee <mayakorinalee@gmail.com>
2016-10-07 09:42:05 +0200

    Add files via upload

---
+++ Cube/Cube.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour {
+	public Transform cube;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			Vector3 pos = cube.position;
+			pos.x += 0.05f;
+			cube.position = pos;
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			Vector3 pos = cube.position;
+			pos.x -= 0.05f;
+			cube.position = pos;
+		} else if (Input.GetKey (KeyCode.UpArrow)) {
+			Vector3 pos = cube.position;
+			pos.y += 0.05f;
+			cube.position = pos;
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
+			Vector3 pos = cube.position;
+			pos.y -= 0.05f;
+			cube.position = pos;
+		}
+	}
+}
---
+++ Cube/NewBehaviourScript.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+	public Transform cube;
+	bool myBool = true;
+	int myInt;
+	string myString;
+	float myFloat;
+	int timer;
+	int timerMax = 5;
+	Vector2 myVector2;
+	Vector3 myVector3;
+	Vector4 myVector4;
+	GameObject myGameObject;
+	UICharInfo myUICharInfo;
+	UILineInfo myUILineInfo;
+	AnimatorStateInfo myAnimStateInfo01;
+	UIVertex myUIVertex;
+
+
+	void Awake (){
+		print (myInt);
+		print ("Blablabla");
+	}
+
+	void Start () {
+		StartCoroutine (TestCoroutine ());
+	}
+
+	IEnumerator TestCoroutine(){
+		while (true) {
+//			print (Time.time);
+//			yield return new WaitForSeconds(1);
+			timer =+1;
+			print (timer);
+			yield return new WaitForSeconds (1);
+			timer =+1;
+			print (timer);
+			yield return new WaitForSeconds (1);
+			timer =+1;
+			print (timer);
+			yield return new WaitForSeconds (1);
+			timer =+1;
+			print (timer);
+			yield return new WaitForSeconds (1);
+			timer =+1;
+			print (timer);
+			yield return new WaitForSeconds (1);
+		}
+
+		if (timer==timerMax) {
+			yield return new WaitForSeconds (1);
+			timer = 0;
+			print (timer);
+		}
+
+	}
+
+	void Update () {
+
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			Vector3 pos = cube.position;
+			pos.x += 0.05f;
+			cube.position = pos;
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			Vector3 pos = cube.position;
+			pos.x -= 0.05f;
+			cube.position = pos;
+		} else if (Input.GetKey (KeyCode.UpArrow)) {
+			Vector3 pos = cube.position;
+			pos.y += 0.05f;
+			cube.position = pos;
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
+			Vector3 pos = cube.position;
+			pos.y -= 0.05f;
+			cube.position = pos;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Tab)) {
+			MyMethod ();
+		}
+		else if (Input.GetKeyUp (KeyCode.Tab)) {
+			MyMethod2 ();
+		}
+	}
+
+	void MyMethod () {
+		myInt = 30;
+		print (myInt);
+	}
+
+	void MyMethod2 () {
+		myInt = 15;
+		print (myInt);
+	}
+}
