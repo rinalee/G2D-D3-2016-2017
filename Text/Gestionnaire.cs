554603ff627854db0b5fac19c5e1681e95e5c440
rinalee <mayakorinalee@gmail.com>
2016-10-07 09:40:50 +0200

    Add files via upload

---
+++ Text/Gestionnaire.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gestionnaire : MonoBehaviour {
+
+//	int compteur = 0;
+	Stats stat;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			InitStat ();
+		}
+		if (Input.GetKeyDown (KeyCode.Return)) {
+			ChangeName ();
+		}
+	}
+		
+	void InitStat(){
+		stat = new Stats(13546,9782,47,3435435,"Acerolaorion",new Vector3(1,2,3));
+	}
+
+	void ChangeName(){
+		stat.SetName ("Heartunderblade");
+	}
+}
---
+++ Text/ImprimerText.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stats{
+//	public ImprimerText(string message){
+//	public ImprimerText(){
+//		Debug.Log (variableImpr);
+//		Debug.Log (message);
+//	}
+
+	int hp;
+	int mp;
+	int level;
+	int xp;
+	string name;
+	Vector3 position;
+
+	public Stats(int hp2, int mp2, int level2, int xp2, string name2, Vector3 position2){
+
+		hp = hp2;
+		mp = mp2;
+		level = level2;
+		xp = xp2;
+		name = name2;
+		position = position2;
+
+		Debug.Log ("HP : " + hp + " MP : " + mp + " Level : " + level + " XP : " + xp + "/10000000" + " Name : " + name);	
+	}
+
+	public void SetName(string newName){
+		name = newName;
+		Debug.Log("New Name = " + name);
+	}
+}
