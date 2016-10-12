ac962249aaa7a2b668665b7955e7a1c57700da69
rinalee <mayakorinalee@gmail.com>
2016-10-12 10:31:17 +0200

    Add files via upload

---
+++ Save Position/Perso.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Perso : MonoBehaviour {
+	public float speed = 10f;
+
+	void Start(){
+		Save.Load ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float f = Input.GetAxis ("Horizontal");	//Save Horizontal Pos
+		GetComponent<Transform>().Translate(Vector3.right * f * Time.deltaTime * speed);	//Translate
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			SaveAction ();
+		}
+	}
+
+	void SaveAction (){
+		Save.DoSave (GetComponent<Transform>().position);	// Get Character Position & Calls Save.cs function
+
+
+	}
+}
---
+++ Save Position/Save.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Save {
+	
+	public static Vector3 Load(){	//Load Position & Sends back to Vector3
+		float x = PlayerPrefs.GetFloat ("posX");
+		float y = PlayerPrefs.GetFloat ("posY");
+		float z = PlayerPrefs.GetFloat ("posZ");
+		Debug.Log ("Load " + x, y, z);
+		return new Vector3 (x, y, z);
+	}
+
+	public static void DoSave(Vector3 positionPlayer){	//Save Position
+		PlayerPrefs.SetFloat ("posX", positionPlayer.x);
+		PlayerPrefs.SetFloat ("posY", positionPlayer.y);
+		PlayerPrefs.SetFloat ("posZ", positionPlayer.z);
+		Debug.Log ("Saved X : " + PlayerPrefs.GetFloat("posX") + " Saved Y : " + PlayerPrefs.GetFloat("posY") + " Saved Z : " + PlayerPrefs.GetFloat("posZ"));	//Show Saved Position in Debug Log
+	}
+}
