cdc204b1f334b8f4975246b87663bef9a853bb0f
rinalee <mayakorinalee@gmail.com>
2016-10-12 10:38:35 +0200

    Update Perso.cs

--- Save Position/Perso.cs
+++ Save Position/Perso.cs
@@ -2,16 +2,16 @@
 using System.Collections;
 
 public class Perso : MonoBehaviour {
-	public float speed = 10f;
+	public float speed = 10f;	// Player Speed
 
 	void Start(){
-		GetComponent<Transform>().position = Save.Load ();
+		GetComponent<Transform>().position = Save.Load ();	// Calls for lastest saved position
 	}
 
 	// Update is called once per frame
 	void Update () {
 		float f = Input.GetAxis ("Horizontal");	//Save Horizontal Pos
-		GetComponent<Transform>().Translate(Vector3.right * f * Time.deltaTime * speed);	//Translate
+		GetComponent<Transform>().Translate(Vector3.right * f * Time.deltaTime * speed);	// Translate
 
 		if (Input.GetKeyDown (KeyCode.Space)) {
 			SaveAction ();
