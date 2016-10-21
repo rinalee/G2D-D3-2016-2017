4b42f727197c2dcaf0d296631229e4574995ffe2
rinalee <mayakorinalee@gmail.com>
2016-10-21 11:55:23 +0200

    Update FocusPoint.cs

--- Character_Rigidbody/FocusPoint.cs
+++ Character_Rigidbody/FocusPoint.cs
@@ -2,14 +2,16 @@ using UnityEngine;
 using System.Collections;
 
 public class FocusPoint : MonoBehaviour {
+	public GameObject target;	// Focus' Target
+	public GameObject player;	// Player's GameObject
 
 	// Use this for initialization
 	void Start () {
-	
+		target = player;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		GetComponent<Transform> ().position = target.GetComponent<Transform> ().position;
 	}
 }
