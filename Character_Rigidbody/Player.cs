cb562e83e73b5dcab8e23b91baea73d0712aa487
rinalee <mayakorinalee@gmail.com>
2016-10-21 09:42:07 +0200

    Add files via upload

---
+++ Character_Rigidbody/Player.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		Movement ();	// Calls Movement Function
+	}
+
+	void Movement (){
+		Rigidbody rigidbody = GetComponent<Rigidbody> ();
+	}
+}
