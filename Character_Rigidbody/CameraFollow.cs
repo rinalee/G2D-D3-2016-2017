63ad4d450a3853d05b6d8fa4b510c657ccc0663b
rinalee <mayakorinalee@gmail.com>
2016-10-21 11:43:51 +0200

    Update CameraFollow.cs

--- Character_Rigidbody/CameraFollow.cs
+++ Character_Rigidbody/CameraFollow.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class CameraFollow : MonoBehaviour {
 	public GameObject focusPoint;
 	public float speedCam = 1f;
+	public float distanceZ = -10f;
 	
 	// Update is called once per frame
 	void Update () {
 		Vector3 v = focusPoint.GetComponent<Transform> ().position;	// Get Focus Position
-		GetComponent<Transform>().position = Vector3.Lerp(GetComponent<Transform>().position, v, Time.deltaTime * speedCam);
+		v.z = distanceZ;
+		Vector3 v2 = GetComponent<Transform>().position;	// Get Camera Position
+		GetComponent<Transform>().position = Vector3.Lerp(v2, v, Time.deltaTime * speedCam);
 	}
 }
