27d7f6140ffbb7ef1149f39f54bc34d96e0562ba
rinalee <mayakorinalee@gmail.com>
2016-10-21 09:58:37 +0200

    Update Player.cs

--- Character_Rigidbody/Player.cs
+++ Character_Rigidbody/Player.cs
@@ -14,6 +14,14 @@ public class Player : MonoBehaviour {
 	}
 
 	void Movement (){
+		float speedX = Input.GetAxis ("Horizontal");	// Save detected input into speedX Variable
+
+		// Apply speedX's float variable to Rigidbody
+
+		// Save player's Velocity in a variable
+		Vector3 velocity = rigidbody.velocity;
+		velocity.x = speedX;	// Apply speedX to Player's velocity
+		rigidbody.velocity = velocity;
 
 	}
 }
