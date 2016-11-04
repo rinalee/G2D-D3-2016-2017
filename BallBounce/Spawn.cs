f9624bb6fee5cf92fc5478f49e52257d382fbb3c
rinalee <mayakorinalee@gmail.com>
2016-11-04 09:37:17 +0100

    Update and rename Bounce.cs to Spawn.cs

---
+++ BallBounce/Spawn.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Spawn : MonoBehaviour {
+	// Variables
+	public GameObject laBalle;
+	public List<GameObject> listBalle;
+
+	// Use this for initialization
+	void Start () {
+		listBalle = new List<GameObject>();
+		// LOOP
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				for (int k = 0; k < 10; k++) {
+					listBalle.Add (Instantiate(laBalle, new Vector3(i,j,k), Quaternion.identity)as GameObject);
+				}
+			}
+		}
+	}
+
+	public class Rewind{
+		public Vector3 position;
+		public Vector3 velocity;
+	}
+
+//	void Update (){
+//		if (Input.GetKeyDown(KeyCode.Space)){
+//			ChangeColor();
+//		}
+//	}
+//	// Random Color & Freeze RigidBody
+//	void ChangeColor(){
+//		for (int i = 0; i < listBalle.Count; i++) {
+//
+//			bool randomColor = Random.Range (0f, 100f) > 20f;
+//			if (randomColor == true) {
+//				listBalle [i].GetComponent<Renderer> ().material.color = Color.gray;
+//				listBalle [i].GetComponent<Rigidbody> ().isKinematic = false;
+//			} else {
+//				listBalle [i].GetComponent<Renderer> ().material.color = Color.black;
+//				listBalle [i].GetComponent<Rigidbody> ().isKinematic = true;
+//			}
+//		}
+//	}
+}
